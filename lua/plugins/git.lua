00869ded75ebb35cf76b868f3511e3f7117b0f7c
William Rice <billyrice12@live.com>
2025-01-12 14:33:18 -0500

    added git related plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        enabled = true,
+        lazy = false,
+        opts = {},
+    },
+    {
+        "sindrets/diffview.nvim",
+    enabled = true,
+    lazy = false,
+        opts = {},
+    },
+    {
+        "f-person/git-blame.nvim",
+        enabled = true,
+        lazy = false,
+        opts = {
+            enabled = false,
+        },
+    },
+}
