513ea3f64959d6f64bff42ed34d0f2f6cf7729e1
William Rice <billyrice12@live.com>
2025-01-12 17:10:16 -0500

    added react snips

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"L3MON4D3/LuaSnip",
+		"mlaursen/vim-react-snippets",
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
 		"onsails/lspkind.nvim",
@@ -20,6 +21,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {},
 		})
+		require("vim-react-snippets").lazy_load()
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local lspkind = require("lspkind")
@@ -41,22 +43,6 @@ return {
 					require("lspconfig")[server_name].setup({
 						capabilities = capabilities,
 					})
-				end,
-
-				zls = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.zls.setup({
-						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-						settings = {
-							zls = {
-								enable_inlay_hints = true,
-								enable_snippets = true,
-								warn_style = true,
-							},
-						},
-					})
-					vim.g.zig_fmt_parse_errors = 0
-					vim.g.zig_fmt_autosave = 0
 				end,
 				["lua_ls"] = function()
 					local lspconfig = require("lspconfig")
