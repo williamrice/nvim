eba5895b3df4af276b668b54253dc0b96c9390d3
William Rice <billyrice12@live.com>
2025-01-12 14:33:45 -0500

    added snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/snacks.nvim",
+	enabled = true,
+	lazy = false,
+	opts = {
+		dashboard = { enabled = true },
+		indent = {
+			enabled = true,
+			chunk = {
+				enabled = true,
+			},
+		},
+		notifier = { enabled = true, timeout = 3000, border = "rounded" },
+	},
+}
