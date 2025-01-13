94a4d9710708def5d2e566fc32730cf608fb2ec5
William Rice <billyrice12@live.com>
2025-01-13 07:49:18 -0500

    started adding intelliphense

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,6 +37,7 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
+				"intelliphense",
 			},
 			handlers = {
 				function(server_name) -- default handler (optional)
