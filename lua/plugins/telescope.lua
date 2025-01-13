b368302ac74c0835f4a2e007ca5876372187a444
William Rice <billyrice12@live.com>
2025-01-13 14:26:14 -0500

    added remote sshfs support

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    lazy = false,
-    enable =true,
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	lazy = false,
+	enable = true,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	init = function()
+		require("telescope").load_extension("remote-sshfs")
+	end,
 }
