97a1a3584d59c409d92edfcea74afffdd1abecd7
William Rice <billyrice12@live.com>
2025-01-13 20:20:28 -0500

    refactor lsp to SRP files

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,65 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"onsails/lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"L3MON4D3/LuaSnip",
+	},
+	opts = function(_, opts)
+		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		opts.sources = opts.sources or {}
+		table.insert(opts.sources, {
+			name = "lazydev",
+			group_index = 0,
+		})
+		return {
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+				{ name = "nvim_lsp_signature_help" },
+			}, {
+				{ name = "buffer" },
+			}),
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "text_symbol", -- show only symbol annotations
+					maxwidth = {
+						-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						-- can also be a function to dynamically calculate max width such as
+						-- menu = function() return math.floor(0.45 *vim.o.columns) end,
+						menu = 50, -- leading text (labelDetails)
+						abbr = 50, -- actual suggestion item
+					},
+					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+					-- The function below will be called before any actual modifications from lspkind
+					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+					before = function(_, vim_item)
+						-- ...
+						return vim_item
+					end,
+				}),
+			},
+		}
+	end,
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,12 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"stevearc/conform.nvim",
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		"L3MON4D3/LuaSnip",
-		"mlaursen/vim-react-snippets",
-		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
-		"onsails/lspkind.nvim",
 	},
-
 	config = function()
-		require("conform").setup({
-			formatters_by_ft = {},
-		})
-		require("vim-react-snippets").lazy_load()
-		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
-		local lspkind = require("lspkind")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
@@ -37,6 +19,7 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
+				"intelephense",
 			},
 			handlers = {
 				function(server_name) -- default handler (optional)
@@ -58,64 +41,204 @@ return {
 						},
 					})
 				end,
-			},
-		})
-
-		----------------------------------------------------------------------
-		-- Cmp configuration
-		----------------------------------------------------------------------
-
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				["intelephense"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.intelephense.setup({
+						root_dir = lspconfig.util.root_pattern("composer.json", ".git", "wp-config.php"),
+						capabilities = capabilities,
+						settings = {
+							intelephense = {
+								files = {
+									maxSize = 5000000,
+								},
+								stubs = {
+									"amqp",
+									"apache",
+									"apcu",
+									"bcmath",
+									"blackfire",
+									"bz2",
+									"calendar",
+									"cassandra",
+									"com_dotnet",
+									"Core",
+									"couchbase",
+									"crypto",
+									"ctype",
+									"cubrid",
+									"curl",
+									"date",
+									"dba",
+									"decimal",
+									"dom",
+									"ds",
+									"enchant",
+									"Ev",
+									"event",
+									"exif",
+									"fann",
+									"FFI",
+									"ffmpeg",
+									"fileinfo",
+									"filter",
+									"fpm",
+									"ftp",
+									"gd",
+									"gearman",
+									"geoip",
+									"geos",
+									"gettext",
+									"gmagick",
+									"gmp",
+									"gnupg",
+									"grpc",
+									"hash",
+									"http",
+									"ibm_db2",
+									"iconv",
+									"igbinary",
+									"imagick",
+									"imap",
+									"inotify",
+									"interbase",
+									"intl",
+									"json",
+									"judy",
+									"ldap",
+									"leveldb",
+									"libevent",
+									"libsodium",
+									"libxml",
+									"lua",
+									"lzf",
+									"mailparse",
+									"mapscript",
+									"mbstring",
+									"mcrypt",
+									"memcache",
+									"memcached",
+									"meminfo",
+									"meta",
+									"ming",
+									"mongo",
+									"mongodb",
+									"mosquitto-php",
+									"mqseries",
+									"msgpack",
+									"mssql",
+									"mysql",
+									"mysql_xdevapi",
+									"mysqli",
+									"ncurses",
+									"newrelic",
+									"oauth",
+									"oci8",
+									"odbc",
+									"openssl",
+									"parallel",
+									"Parle",
+									"pcntl",
+									"pcov",
+									"pcre",
+									"pdflib",
+									"PDO",
+									"pdo_ibm",
+									"pdo_mysql",
+									"pdo_pgsql",
+									"pdo_sqlite",
+									"pgsql",
+									"Phar",
+									"phpdbg",
+									"posix",
+									"pspell",
+									"pthreads",
+									"radius",
+									"rar",
+									"rdkafka",
+									"readline",
+									"recode",
+									"redis",
+									"Reflection",
+									"regex",
+									"rpminfo",
+									"rrd",
+									"SaxonC",
+									"session",
+									"shmop",
+									"SimpleXML",
+									"snmp",
+									"soap",
+									"sockets",
+									"sodium",
+									"solr",
+									"SPL",
+									"SplType",
+									"SQLite",
+									"sqlite3",
+									"sqlsrv",
+									"ssh2",
+									"standard",
+									"stats",
+									"stomp",
+									"suhosin",
+									"superglobals",
+									"svn",
+									"sybase",
+									"sync",
+									"sysvmsg",
+									"sysvsem",
+									"sysvshm",
+									"tidy",
+									"tokenizer",
+									"uopz",
+									"uv",
+									"v8js",
+									"wddx",
+									"win32service",
+									"winbinder",
+									"wincache",
+									"wordpress",
+									"xcache",
+									"xdebug",
+									"xhprof",
+									"xml",
+									"xmlreader",
+									"xmlrpc",
+									"xmlwriter",
+									"xsl",
+									"xxtea",
+									"yaf",
+									"yaml",
+									"yar",
+									"zend",
+									"Zend OPcache",
+									"ZendCache",
+									"ZendDebugger",
+									"ZendUtils",
+									"zip",
+									"zlib",
+									"zmq",
+									"zookeeper",
+								},
+								environment = {
+									includePaths = {
+										"/usr/include/php",
+										"~/.composer/vendor",
+									},
+								},
+							},
+						},
+					})
 				end,
 			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
-				{ name = "nvim_lsp_signature_help" },
-			}, {
-				{ name = "buffer" },
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol", -- show only symbol annotations
-					maxwidth = {
-						-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-						-- can also be a function to dynamically calculate max width such as
-						-- menu = function() return math.floor(0.45 * vim.o.columns) end,
-						menu = 50, -- leading text (labelDetails)
-						abbr = 50, -- actual suggestion item
-					},
-					ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-					show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-					-- The function below will be called before any actual modifications from lspkind
-					-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-					before = function(_, vim_item)
-						-- ...
-						return vim_item
-					end,
-				}),
-			},
 		})
-
 		vim.diagnostic.config({
 			-- update_in_insert = true,
 			float = {
 				focusable = false,
 				style = "minimal",
 				border = "rounded",
-				source = "always",
+				source = "if_many",
 				header = "",
 				prefix = "",
 			},
