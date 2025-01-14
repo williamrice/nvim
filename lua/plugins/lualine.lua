ee74a313325feff76e59495734386e007d85724a
William Rice <billyrice12@live.com>
2025-01-13 21:10:07 -0500

    add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		enabled = true,
+		lazy = false,
+		opts = {
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						newfile_status = true,
+						path = 1,
+						shorting_target = 40,
+						symbols = {
+							modified = "[+]",
+							readonly = "[RO]",
+							unnamed = "[No Name]",
+							newfile = "[New]",
+						},
+					},
+				},
+			},
+		},
+	},
+}
