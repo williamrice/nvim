5872b517d409f6615a49125566147339d330754d
William Rice <william@williamarice.com>
2025-01-28 22:05:48 -0500

    added check to disable discord plugin on linux

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,7 +1,13 @@
+local should_enable = true
+local os_name = vim.loop.os_uname().sysname
+if os_name == 'Linux' or os_name:match 'BSD$' then
+	should_enable = false
+end
 return {
 	"vyfor/cord.nvim",
 	branch = "client-server",
 	build = ":Cord update",
+	enabled = should_enable,
 	opts = {
 		display = {
 			swap_fields = true,
