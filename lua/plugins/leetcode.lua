0ad26f23e95bffa1f1a5d1782f07efe8e5209451
William Rice <william@williamarice.com>
2025-01-28 21:55:55 -0500

    added leetcode nvim

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,16 @@
+local leet_arg = "leetcode.nvim"
+return {
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		-- "ibhagwan/fzf-lua",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = leet_arg ~= vim.fn.argv(0, -1),
+	opts = {
+		arg = leet_arg,
+		lang = "python3"
+	},
+}
