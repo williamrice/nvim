dc762b301063dd57133fc63127f68ff400280798
William Rice <william@williamarice.com>
2025-06-14 13:52:47 -0400

    updated config

--- lua/plugins/codecopy.lua
+++ lua/plugins/codecopy.lua
@@ -4,11 +4,22 @@ return {
 	enabled = true,
 	lazy = true,
 	event = "VeryLazy",
-	debug = false,
 	opts = {
 		notify = true,
+		openui = true,
+		debug = true,
+		include_file_path = true,
 		env = {
 			enabled = true,
+		},
+		webhook = {
+			branding = true,
+			embed = true,
+			author = {
+				name = "Zonric",
+				profile = "https://www.github.com/Zonric",
+			},
+			url = "https://discord.com/api/webhooks/1381636144353251509/4a3SjHGmnMhGZ4xRlptrMM2Gt5qQ3-7cdUEnwxKXKT_8nhpxUGCBoI0J93vwT1w-TAlL",
 		},
 	},
 }
