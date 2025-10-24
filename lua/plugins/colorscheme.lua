6bd34601cfadfae15fbaa77536862ec0ab46fc56
William Rice <william@williamarice.com>
2025-10-24 18:08:32 -0400

    fix colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return {
 	lazy = false,
 	priority = 999,
 	config = function()
-		vim.cmd.colorscheme("Carbonfox")
+		vim.cmd.colorscheme("carbonfox")
+		vim.api.nvim_set_hl(0, "@punctuation.bracket.php", { fg = "#cb9405", bold = true })
 	end,
 }
