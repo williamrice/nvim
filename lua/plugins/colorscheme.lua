f88ee89606ae7f5430b46e623d2afd48e952ff26
William Rice <william@williamarice.com>
2025-10-24 21:05:12 -0400

    better visual select

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
 	priority = 999,
 	config = function()
 		vim.cmd.colorscheme("carbonfox")
+		vim.api.nvim_set_hl(0, "Visual", { bg = "#235c63", fg = "NONE" })
 		vim.api.nvim_set_hl(0, "@punctuation.bracket.php", { fg = "#cb9405", bold = true })
 	end,
 }
