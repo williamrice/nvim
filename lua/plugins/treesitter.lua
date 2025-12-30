692ece19460db4b2a200980c29847b50273d15b4
William Rice <william@williamarice.com>
2025-12-30 17:59:00 -0500

    fix treesitter text objects issue

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,68 +2,67 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			branch = "main"
+		}
 	},
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = {
-				"astro",
-				"bash",
-				"c_sharp",
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"elixir",
-				"heex",
-				"javascript",
-				"html",
-				"css",
-				"tsx",
-				"json",
-				"php",
-				"typescript",
-				"markdown",
-				"markdown_inline",
-				"yaml",
-				"toml",
-				"dockerfile",
-				"gitignore",
+	opts = {
+		ensure_installed = {
+			"astro",
+			"bash",
+			"c_sharp",
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"elixir",
+			"heex",
+			"javascript",
+			"html",
+			"css",
+			"tsx",
+			"json",
+			"php",
+			"typescript",
+			"markdown",
+			"markdown_inline",
+			"yaml",
+			"toml",
+			"dockerfile",
+			"gitignore",
+		},
+		sync_install = false,
+		auto_install = true,
+		ignore_install = {},
+		modules = {},
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
 			},
-			sync_install = false,
-			auto_install = true,
-			ignore_install = {},
-			modules = {},
-			highlight = {
+		},
+		textobjects = {
+			select = {
 				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
+				lookahead = true,
 				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
 				},
 			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-			},
-		})
-	end,
+		},
+	}
 }
