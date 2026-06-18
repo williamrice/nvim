6bedd3f76755cf0a8c7b2c08803203a9727639c4
William Rice <william@williamarice.com>
2026-06-17 22:35:37 -0400

    deleted twig ls stuff

--- after/ftplugin/twig.lua
+++
@@ -1,7 +0,0 @@
-vim.lsp.enable("twiggy_language_server")
-vim.lsp.enable("html")
-
-vim.treesitter.start()
-vim.bo.autoindent = true
-vim.bo.smartindent = true
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
